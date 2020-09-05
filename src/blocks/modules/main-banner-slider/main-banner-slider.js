0bc4dcd028c2af329283084b59f74c01f322fe53
RadioMan116 <noris75564@gmail.com>
2020-09-05 17:34:30 +0300

    '2'

--- src/blocks/modules/main-banner-slider/main-banner-slider.js
+++ src/blocks/modules/main-banner-slider/main-banner-slider.js
@@ -1,14 +1,21 @@
-// import Swiper, {Pagination} from "swiper";
-// // import "swiper/swiper-bundle.css";
+(() => {
+    console.log("1");
+})();
+import Swiper, { Pagination } from "swiper";
+import "swiper/swiper-bundle.css";
 
-// (function() {
 
-//     var mainBannerSlider = new Swiper(".swiper-container.main-banner__slider", {
-//         // If we need pagination
-//         pagination: {
-//             el: ".swiper-pagination",
-//         },
-//     });
 
-//     console.log("1");
-// })();
+
+// configure Swiper to use modules
+Swiper.use([Pagination]);
+
+(function() {
+    const mainBannerSlider = new Swiper(".swiper-container.main-banner__slider", {
+        // If we need pagination
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+    console.log("1");
+})();
